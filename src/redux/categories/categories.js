744d41cbca1bafa3ddf480a1239591ab55f3a741
Gasangwa Thomas <thomasgasangwa@gmail.com>
2022-07-19 15:28:41 +0200

    check the status of the category

--- src/redux/categories/categories.js
+++ src/redux/categories/categories.js
@@ -1,3 +1,14 @@
-const STATUS = () => {
-    
-}
+export const statusAction = () => ({
+  type: 'STATUS',
+});
+
+const checkingStatus = (state = [], action) => {
+  if (action.type === 'STATUS') {
+    return {
+      STATUS: 'Under construction',
+    };
+  }
+  return state;
+};
+
+export default checkingStatus;
